84926e2d038f2f42b81e25794912eca04d688f11
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-07 23:52:55 +0700

    Data Entry Uploads Image with Grid basic

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { Button, Row, Col } from "antd";
 import "./App.css";
 
 import "antd/dist/antd.less";
 
 import TypographyAll from "./Component/General/TypographyAll";
+import UploadImages from "./Component/DataEntry/Upload";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Button type="primary">Button</Button>
-                <TypographyAll></TypographyAll>
-            </div>
-        );
-    }
+   render() {
+      return (
+         <div className="App">
+            <Row gutter={[8, 8]} type="flex" justify="center" align="top">
+               <Col sm={4} md={12} >
+                  <Button type="primary">Button</Button>
+               </Col>
+               <Col  sm={4} md={12} >
+                  <TypographyAll></TypographyAll>
+               </Col>
+            </Row>
+            <Row gutter={[8, 8]} type="flex" justify="center" align="top">
+               <Col sm={6} md={12}>
+                  <UploadImages></UploadImages>
+               </Col>
+            </Row>
+         </div>
+      );
+   }
 }
 
 export default App;
